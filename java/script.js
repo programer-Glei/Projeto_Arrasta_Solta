f5c26f5cb6b6a510712d83f2f491806e16d7474b
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-07-27 20:26:21 -0300

    proximo comit

--- java/script.js
+++ java/script.js
@@ -3,3 +3,13 @@ document.querySelectorAll('.item').forEach(item =>{
     item.addEventListener('dragstart', dragStart);
     item.addEventListener('dragend', dragEnd);
 })
+
+
+//functions item
+function dragStart(e){
+    e.currentTarget.classList.add('dragging')
+}
+
+function dragEnd(e){
+    e.currentTarget.classList.remove('dragging')
+}
